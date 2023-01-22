692722c0c090760879e7f418c73ebbac50b087d1
Tatiana Osipova <otv120780@gmail.com>
2023-01-22 14:18:24 -0500

    feat: add form for message thaks for booking

--- js/script.js
+++ js/script.js
@@ -225,7 +225,7 @@ const renderTime = (wrapper, data) => {
 
 const initReserve = () => {
     const reserveForm = document.querySelector('.reserve__form');
-    const { fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn } = 
+    const { fieldservice, fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn } = 
         reserveForm;
     
     addDisabled([fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn]);
@@ -292,6 +292,33 @@ const initReserve = () => {
         if(target.name === 'time') {
             removeDisabled([btn]);
         }
+    });
+
+    reserveForm.addEventListener('submit', async(e) => {
+        e.preventDefault();
+
+        const formData = new FormData(reserveForm);
+        const json = (JSON.stringify(Object.fromEntries(formData)));    
+
+        const responce = await fetch(`${API_URL}api/order`, {
+            method: 'post',
+            body: json,
+        });
+
+        const data = await responce.json();
+        
+        addDisabled([fieldservice, fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn]);
+
+        const p = document.createElement('p');
+        p.textContent = `
+            Спасибо за бронирование #${data.id}!
+            Ждем Вас ${new Intl.DateTimeFormat('ru-RU', {
+                month: 'long',
+                day: 'numeric',                
+            }).format(new Date(`${data.month}/${data.day}`))},
+            время ${data.time}       
+        `;
+        reserveForm.append(p);
     });
 };
 
